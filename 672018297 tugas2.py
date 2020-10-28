eeb0719bb55878ffc4149bac88d5176895682dcb
Vicky AS <65887817+vick1208@users.noreply.github.com>
2020-10-28 14:03:41 +0700

    Add files via upload

---
+++ 672018297 tugas2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 28 11:15:42 2020
+
+@author: vicky
+"""
+#%%
+import pandas as pd
+datamcd = pd.read_csv("E:\Machine Learning\menu.csv")
+print(datamcd)
